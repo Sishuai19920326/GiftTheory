4628d653a24116a746fbea195f18c4df2181f8c7
Sishuai <919976766@qq.com>
2017-02-11 08:44:09 +0800

    第一次测试提交推送

--- GiftTheory/app/src/main/java/com/lanou3g/gifttheory/MainActivity.java
+++ GiftTheory/app/src/main/java/com/lanou3g/gifttheory/MainActivity.java
@@ -2,12 +2,13 @@ package com.lanou3g.gifttheory;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+//我是司帅
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
